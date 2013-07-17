ed23213796c2784b5c97f9cc7f705cd0428bf776
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-07-18 00:25:35 +1000

    Started plugin registry

---
+++ tyrant/plugreg.py
@@ -0,0 +1,11 @@
+"""Plugin registry."""
+__author__ = 'Taylor "Nekroze" Lawson'
+__email__ = 'nekroze@eturnilnetwork.com'
+
+
+PLUGINS = {}
+
+
+def register_plugin(plugin):
+    """Register a plugin."""
+    PLUGINS.append(plugin.codename, plugin)
