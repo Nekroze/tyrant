39cd36c8362e9d2ac96bf6e8cfe90c75a602a2a1
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-07-28 11:29:12 +1000

    Reorganized and documented Command to support a recursive command system

--- tyrant/command.py
+++ tyrant/command.py
@@ -1,10 +1,20 @@
-"""Tyrant command interface."""
+"""
+To create a new command for **Tyrant** the ``Command`` class needs to be
+subclassed for your command and override the ``Command.execute`` method to
+provide your command execution.
+"""
 from __future__ import print_function
 __author__ = 'Taylor "Nekroze" Lawson'
 __email__ = 'nekroze@eturnilnetwork.com'
 
 
 class Command(object):
+    """
+    A command eases implementation of a **Tyrant** command.
+
+    ``Command`` requires a command name to be used to call this command and a
+    description that describes this commands usage.
+    """
     def __init__(self, name, description):
         self.name = name
         self.description = description
@@ -19,13 +29,21 @@ class Command(object):
     def __len__(self):
         return len(self.commands)
 
+    def __call__(self, args):
+        if args and args[0] in self.commands:
+                return self.commands[args[0]](args[1:])
+        self.execute(args)
+
+    def add_command(self, command):
+        """
+        Add a subcommand to this command. Must be an instance of ``Command``.
+        """
+        assert isinstance(command, Command)
+        self.commands[command.name] = command
+
     def execute(self, args):
         """Execute the given command."""
-        if args:
-            if args[0] in self.commands:
-                self.commands[args[0]](args[1:])
-            else:
-                pass
+        assert False, "{0} execution is not yet implemented.".format(self.name)
 
     def help(self, args):
         """Display command usage help."""
@@ -36,4 +54,4 @@ class Command(object):
             print(self.description + '\n')
 
             for key in sorted(self.map.keys()):
-                print("{0} - {1}".format(key, self.commands[key].__docs__))
+                print("{0} - {1}".format(key, self.commands[key].description))
